4cd14be67a5d06843bae7584fb4c164045bcc4a9
Ziga Vukcevic <ziga@be-codified.com>
2020-03-27 22:00:13 +0100

    Update

--- 01-class-animals-zoo/src/somePackage/Zoo.java
+++ 01-class-animals-zoo/src/somePackage/Zoo.java
@@ -3,14 +3,25 @@ package somePackage;
 public class Zoo {
   // This is entry point of Java program
   public static void main(String args[]) {
+    // Dog and duck (created by Animal class)
     Animal dog = new Animal("dog", "woof woof", 4);
     Animal duck = new Animal("duck", "ga ga", 2);
 
     dog.speak();
+    dog.eat();
+
+    System.out.println("---");
+
+    duck.speak();
     duck.eat();
 
+    System.out.println("---");
+
+    // Fish (created by Fish class which extends Animal class)
     Fish fish = new Fish("fish", "bloob bloob", 0);
 
-    fish.swim();
+    fish.speak(); // Method from Animal class
+    fish.eat(); // Method from Animal class
+    fish.swim(); // Method from Fish class
   }
 }
